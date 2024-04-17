6ed9da42a16dff6b4a2d0703ca6a34b2002a09e5
samuel_natividade <samuell.natividade@gmail.com>
2024-04-16 22:07:19 -0300

    teste

---
+++ exemplo2.py
@@ -0,0 +1,12 @@
+# crie um programa que o usuario digite 2 valores e retorne a soma deles
+valor_1 = int(input("digite o valor_1: "))
+valor_2 = int(input("digite o valor_2: "))
+print(valor_1 + valor_2)
+
+
+numero = 3 # int
+numero_decimal = 4.78 # float
+nome_de_usuario = "Samuel" #float
+verdadeiro = True # boolean
+
+print(type(numero))
